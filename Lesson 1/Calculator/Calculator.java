08709c55f4c647b7e97c14c51add114c902dd508
Vladimir Moshnogorskiy <il75ich@yandex.ru>
2022-03-25 16:15:04 +0300

    Исправление по итоговому заданию Lesson 1

--- Lesson 1/Calculator/Calculator.java
+++ Lesson 1/Calculator/Calculator.java
@@ -1,38 +1,33 @@
 public class Calculator {
     public static void main(String[] args) {
-        char sign = '^';
+        char sign = '/';
         int num1 = 10;
         int num2 = 5;
         int result = 0;
 
         switch (sign) {
-            case ('+'): 
+            case ('+'):
                 result = num1 + num2;
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
                 break;
             case ('-'):
                 result = num1 - num2;
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
                 break;
             case ('*'):
                 result = num1 * num2;
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
                 break;
             case ('/'):
                 result = num1 / num2;
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
                 break;
             case ('%'):
                 result = num1 % num2;
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
                 break;
             case ('^'):
                 result = num1;
                 for (int i = 0; i < num2 - 1; i++) {
-                    result = result * num1;
+                    result *= num1;
                 }
-                System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
                 break;
         }
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
     }
 }
---
+++ Lesson 1/GuessNumberGame/GuessNumberGame.java
@@ -0,0 +1,19 @@
+public class GuessNumberGame {
+    public static void main(String[] args) {
+        int secretNum = 11;
+        int gamerNum = 50;
+
+        while (gamerNum != secretNum) {
+            if (gamerNum < secretNum) {
+                System.out.println("Данное число " + gamerNum +" меньше того, что загадал компьютер");
+              gamerNum += 2;
+            } else if (gamerNum > secretNum) {
+               System.out.println("Данное число " + gamerNum + " больше того, что загадал компьютер");
+               gamerNum--;
+            }
+        }
+        if (gamerNum == secretNum) {
+                System.out.println("Вы победили! Загаданное число: " + gamerNum);
+            }
+    }
+}
